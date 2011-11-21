285375657f434d1d2d8e1a862b565fc366e26568
htamura <light@jp.ibm.com>
2011-11-21 18:28:14 +0900

    modified:   module.js 	modified:   robot/Animation.html 	modified:   robot/Animation2.html 	modified:   robot/ButtonList.html 	modified:   robot/ButtonList2.html 	modified:   robot/Flippable.html 	modified:   robot/Icon.html 	modified:   robot/Icon2.html 	modified:   robot/ListItem.html 	modified:   robot/ScrollableView.html 	modified:   robot/ScrollableView2.html 	modified:   robot/ScrollableView3.html 	modified:   robot/Settings.html 	modified:   robot/Switch.html 	modified:   robot/Switch2.html 	modified:   robot/TabBar.html 	modified:   robot/TabBar2.html 	modified:   robot/module.js 	robot/index.html

--- tests/module.js
+++ tests/module.js
@@ -3,7 +3,7 @@ dojo.provide("dojox.mobile.tests.module");
 try{
 	dojo.require("dojox.mobile.tests.doh.module");
 	if(!dojo.isBB && !dojo.isAndroid && !dojo.isIPhone && !dojo.isIPad && !dojo.isIPod) {
-//		dojo.require("dojox.mobile.tests.robot.module");
+		dojo.require("dojox.mobile.tests.robot.module");
 	}
 
 }catch(e){
