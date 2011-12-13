c04737bbbb71c1230acbaef430fdd1d45241a483
htamura <light@jp.ibm.com>
2011-12-13 16:33:48 +0900

    modified:   TabBar.js 	new file:   ToolBarButtonTests.html

--- tests/doh/TabBar.js
+++ tests/doh/TabBar.js
@@ -9,17 +9,17 @@ dojo.addOnLoad(function(){
 					var demoWidget = dijit.byId("dojox_mobile_TabBar_0");
 					doh.assertEqual('mblTabBar mblTabBarSegmentedControl', demoWidget.domNode.className);
 
-					verifyTabBarButton("dojox_mobile_TabBarButton_0", 'New', "mblTabBarButton mblTabBarButtonSelected", 'hidden', '', /tab-icon-16.png/i, /tab-icon-16h.png/);
+					verifyTabBarButton("dojox_mobile_TabBarButton_0", 'New', "mblTabBarButton mblTabBarButtonHasIcon mblTabBarButtonSelected", 'hidden', '', /tab-icon-16.png/i, /tab-icon-16h.png/);
 
 					demoWidget = dijit.byId("dojox_mobile_TabBar_1");
 					doh.assertEqual('mblTabBar mblTabBarTabBar', demoWidget.domNode.className);
 
-					verifyTabBarButton("dojox_mobile_TabBarButton_3", 'New', "mblTabBarButton mblTabBarButtonSelected", 'hidden', '', /tab-icon-16.png/i, /tab-icon-16h.png/);
+					verifyTabBarButton("dojox_mobile_TabBarButton_3", 'New', "mblTabBarButton mblTabBarButtonHasIcon mblTabBarButtonSelected", 'hidden', '', /tab-icon-16.png/i, /tab-icon-16h.png/);
 
 					demoWidget = dijit.byId("dojox_mobile_TabBar_2");
 					doh.assertEqual('mblTabBar mblTabBarTabBar', demoWidget.domNode.className);
 
-					verifyTabBarButton("dojox_mobile_TabBarButton_6", 'Featured', "mblTabBarButton mblTabBarButtonSelected", 'hidden', '', /tab-icons.png/i, /tab-icons.png/i, true);
+					verifyTabBarButton("dojox_mobile_TabBarButton_6", 'Featured', "mblTabBarButton mblTabBarButtonHasIcon mblTabBarButtonSelected", 'hidden', '', /tab-icons.png/i, /tab-icons.png/i, true);
 					demoWidget = dijit.byId("dojox_mobile_TabBarButton_6");
 					verifyRect(demoWidget.iconNode1.childNodes[0], "0px", "29px", "29px", "0px");
 					verifyRect(demoWidget.iconNode2.childNodes[0], "29px", "29px", "58px", "0px");
@@ -40,12 +40,19 @@ dojo.addOnLoad(function(){
 					demoWidget.set({label:"New Value"});
 					demoWidget.select();
 
-					verifyTabBarButton("dojox_mobile_TabBarButton_2", 'New Value', "mblTabBarButton mblTabBarButtonSelected", 'hidden', '', /tab-icon-10.png/i, /tab-icon-10h.png/);
+					verifyTabBarButton("dojox_mobile_TabBarButton_2", 'New Value', "mblTabBarButton mblTabBarButtonHasIcon mblTabBarButtonSelected", 'hidden', '', /tab-icon-10.png/i, /tab-icon-10h.png/);
 					demoWidget = dijit.byId("dojox_mobile_TabBarButton_5");
 					demoWidget.set({label:"New Value", icon1:"../images/tab-icon-11.png", icon2:"../images/tab-icon-11h.png"});
 					demoWidget.select();
 
-					verifyTabBarButton("dojox_mobile_TabBarButton_5", 'New Value', "mblTabBarButton mblTabBarButtonSelected", 'hidden', '', /tab-icon-11.png/i, /tab-icon-11h.png/);				}),500);
+					verifyTabBarButton("dojox_mobile_TabBarButton_5", 'New Value', "mblTabBarButton mblTabBarButtonHasIcon mblTabBarButtonSelected", 'hidden', '', /tab-icon-11.png/i, /tab-icon-11h.png/)
+
+					demoWidget = dijit.byId("dojox_mobile_TabBarButton_4");
+					demoWidget.set({icon1:null, icon2:null});
+					doh.assertEqual(null, demoWidget.iconNode1, demoWidget.domNode.id);
+					doh.assertEqual(null, demoWidget.iconNode2, demoWidget.domNode.id);
+
+				}),500);
 				return d;
 			}
 		}
