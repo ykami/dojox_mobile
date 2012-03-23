2cb3609a8936c0d80e8dafd655979af6cc4e7b30
htamura <light@jp.ibm.com>
2012-03-23 12:10:12 +0900

    new file:   SpinWheel_Programmatic.html 	modified:   index.html 	modified:   module.js

--- tests/doh/module.js
+++ tests/doh/module.js
@@ -44,10 +44,10 @@ try{
 	doh.registerUrl("dojox.mobile.tests.doh.View", dojo.moduleUrl("dojox.mobile", "tests/doh/View-demo.html"),999999);
 	doh.registerUrl("dojox.mobile.tests.doh.URLProperty", dojo.moduleUrl("dojox.mobile", "tests/doh/TestURLProp.html"),999999);
 	doh.registerUrl("dojox.mobile.tests.doh.URLProperty", dojo.moduleUrl("dojox.mobile", "tests/doh/TestURLProp2.html"),999999);
+	doh.registerUrl("dojox.mobile.tests.doh.SpinWheel", dojo.moduleUrl("dojox.mobile", "SpinWheel_Programmatic.html"),999999);
 
 	doh.registerUrl("dojox.mobile.tests.doh.Button", dojo.moduleUrl("dojox.mobile", "tests/doh/ButtonTests.html"),999999);
 	doh.registerUrl("dojox.mobile.tests.doh.CheckBox", dojo.moduleUrl("dojox.mobile", "tests/doh/CheckBoxTests.html"),999999);
-	doh.registerUrl("dojox.mobile.tests.doh.ComboBoxTests", dojo.moduleUrl("dojox.mobile", "tests/doh/ComboBoxTests.html"),999999);
 	if(!dojo.isIE){
 		doh.registerUrl("dojox.mobile.tests.doh.ContentPane", dojo.moduleUrl("dojox.mobile", "tests/doh/ContentPaneTests.html"),999999);
 		doh.registerUrl("dojox.mobile.tests.doh.EdgeToEdgeDataList", dojo.moduleUrl("dojox.mobile", "tests/doh/EdgeToEdgeDataListTests.html"),999999);
